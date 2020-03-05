37ace956b75758289276ee66bcf03db2ce492ae8
Loup-Rodolphe THIBAULT <loupthibault@gmail.com>
2020-03-05 12:43:43 +0100

    :pencil: Add typescripts docs

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+
+export type BrowserVersion = {
+    name: String;
+    version: String;
+};
+
+export type BrowserInfos = {
+    browserVersion: BrowserVersion;
+
+    isOsx: Boolean;
+    isIos: Boolean;
+    isIpad: Boolean;
+    isIphone: Boolean;
+    iosVersion?: Number;
+
+    isWindows: Boolean;
+    isWindowsPhone: Boolean;
+    isWindowsTablet: Boolean;
+
+    isAndroid: Boolean;
+    isAndroidPhone: Boolean;
+    isAndroidTablet: Boolean;
+
+    isTablet: Boolean;
+    isPhone: Boolean;
+    isDesktop: Boolean;
+    isDevice: Boolean;
+    isTouch: Boolean;
+
+    isFirefox: Boolean;
+    isSafari: Boolean;
+    isOpera: Boolean;
+    isIE: Boolean;
+    isIE9: Boolean;
+    isIE10: Boolean;
+    isIE11: Boolean;
+    isIE12: Boolean;
+    isEdge: Boolean;
+};
+
+type VacuumBrowser = BrowserInfos & {
+    addClasses(el: HTMLElement): void;
+    getInfos(): BrowserInfos;
+};
+
+export default VacuumBrowser;
